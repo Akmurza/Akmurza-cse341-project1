f6711a18653660b5d10f56aeebe818afba2281f8
Akmurza <akuklin@byupathway.edu>
2026-03-19 05:12:56 +0400

    week 2 additional error

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', routes);
 
+app.use((error, req, res, next) => {
+  if (error instanceof SyntaxError && error.status === 400 && 'body' in error) {
+    return res.status(400).json({
+      message: 'Invalid JSON in request body',
+    });
+  }
+
+  return next(error);
+});
+
 mongodb.initDb((error) => {
   if (error) {
     console.error('Database failed:', error);
